e5cabc7767ab3b1a4f93738c2c0343f6c2698cd6
Felipe <bollinger.felipe@gmail.com>
2025-09-23 20:43:14 -0300

    last_version of 23-9

--- main.py
+++ main.py
@@ -62,20 +62,23 @@ if __name__ == "__main__":
     Funciones.mover_con_pid_sin_reiniciar(100, -180) #baja el panel solar
     Funciones.subir_brazo(85)                        #sube el brazo
     Funciones.turn_to_angle(90)                      #
-    Funciones.mover_con_pid_sin_reiniciar(200, -90)  #avanza a la mitad de las pelotas y la caja
+    Funciones.mover_con_pid_sin_reiniciar(170, -90)  #avanza a la mitad de las pelotas y la caja
     Funciones.turn_to_angle(90)                      #
     Funciones.move_distance_cm(100, 200)             #agarra 3 bloques como max y va hasta el fondo
     Funciones.turn_to_angle(-90)                     #Se acomoda para llevar las pelotas
-    Funciones.move_distance_cm(30, 200)              #'''
+    Funciones.move_distance_cm(25, 200)              #'''
     Funciones.turn_to_angle(90)                      #'''
-    Funciones.move_distance_cm(45, 300)              #deposita los tres bloques
-    Funciones.move_distance_cm(-55, 400)            #Retrocede un metro para agarrar los otros tres bloques
+    Funciones.move_distance_cm(57, 300)              #deposita los tres bloques
+    Funciones.move_distance_cm(-75, 400)             #Retrocede un metro para agarrar los otros tres bloques
     Funciones.turn_to_angle(90)
     Funciones.move_distance_cm(80, 200)
     Funciones.turn_to_angle(90)
-    Funciones.move_distance_cm(90, 300)
-    Funciones.move_distance_cm(-95, 300)
+    Funciones.move_distance_cm(100, 300)
+    Funciones.move_distance_cm(-100, 300)
     Funciones.turn_to_angle(90)
     Funciones.move_distance_cm(-10, 200)
-    Funciones.move_distance_cm(40, 200)
-    motor_derecho.run_angle(200, 360)
+    Funciones.move_distance_cm(30, 200)
+    Funciones.turn_to_angle(-90)
+    Funciones.move_distance_cm(20, 200)
+    motor_derecho.run_angle(400, 1800)
+    Funciones.move_distance_cm(100, 300)
