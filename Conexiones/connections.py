05e2d415fc2acbc8f724ded4e02782977e6a36c6
Tomas Arganaraz <tomiargacaba@gmail.com>
2025-09-23 20:12:35 -0300

    fix

--- Conexiones/connections.py
+++ Conexiones/connections.py
@@ -9,6 +9,12 @@ from pybricks.robotics import DriveBase           # Control de movimiento tipo c
 from pybricks.iodevices import I2CDevice
 from pixycamev3.pixy2 import Pixy2
 
+# Variables globales para las pociciones de las cajas
+box_amarillo=0
+box_roja=0
+box_blanca=0
+box_verde=0
+
 # Camarita Pixycam
 pixy2 = Pixy2(port=4, i2c_address=0x54)
 
