d0af09a1bb1ca62e80570ea512dc75a55e11f1c7
Felipe <bollinger.felipe@gmail.com>
2025-09-23 21:09:26 -0300

    update

--- Tomi_principal/segundo.py
+++ Tomi_principal/segundo.py
@@ -15,27 +15,24 @@ if __name__ == "__main__":
     Funciones.mover_con_pid_sin_reiniciar(-90,-180)         #retrocede llendo hacia el rover
     Funciones.bajar_brazo(95)                               #baja el brazo
     Funciones.mover_con_pid_sin_reiniciar(100,-180)         #avanza para bajar el ala del rover
-    Funciones.bajar_pala(230)                               #baja la pala
+    Funciones.bajar_pala(240)                               #baja la pala
     Funciones.subir_brazo(85)                               #sube el brazo
     Funciones.mover_con_pid_sin_reiniciar(180,-180)         #avanza a juntar la primera pelota
     Funciones.mover_con_pid_sin_reiniciar(-120,-180)        #retrocede para acomodar
-    Funciones.subir_pala(100)                               #sube pala y acomoda la pelota
-    Funciones.bajar_pala(100)                               #vuelve a bajar pala
+    Funciones.subir_pala(80)                                #sube pala y acomoda la pelota
+    Funciones.bajar_pala(80)                                #vuelve a bajar pala
     Funciones.mover_con_pid_sin_reiniciar(180,-180)         #va a juntar la otra pelota
     Funciones.mover_con_pid_sin_reiniciar(-150,-180)        #retrocede para irse a dejarlas
-    Funciones.subir_pala(95)                                #sube pala para que no se caigan las pelotas
-    Funciones.giro_der(-90)                                 #gira 90 a la derecha
-    Funciones.mover_con_pid_sin_reiniciar(400,-90)          #avanza a dejar las pelotas
-    Funciones.giro_izq(0)                                   #gira 0 a la izquerda
-    Funciones.mover_con_pid_sin_reiniciar(200,0)            #avanza para despues abrir caja
+    Funciones.subir_pala(70)                                #sube pala para que no se caigan las pelotas
+    Funciones.turn_to_angle(90)                             #gira 90 a la derecha
+    Funciones.mover_con_pid_sin_reiniciar(370,-90)          #avanza a dejar las pelotas
+    Funciones.turn_to_angle(90)                             #gira 0 a la izquerda
+    Funciones.mover_con_pid_sin_reiniciar(15,0)             #avanza para despues abrir caja
     Funciones.bajar_brazo(95)                               #baja el brazo
     Funciones.mover_con_pid_sin_reiniciar(-180,0)           #abre la caja
     Funciones.subir_brazo(30)                               #abre la caja
     Funciones.mover_con_pid_sin_reiniciar(-50,0)            #abre la caja
     Funciones.subir_brazo(30)                               #abre la caja
-    Funciones.mover_con_pid_sin_reiniciar(-60,0)            #abre la caja
-    Funciones.subir_brazo(30)                               #abre la caja
-    #Funciones.mover_con_pid_sin_reiniciar(-50,0)           #por si acaso falta abrir cajita
     Funciones.subir_pala(130)                               #deposita las pelotitas
     Funciones.mover_con_pid_sin_reiniciar(200,0)            #se aleja 20 cm
     Funciones.giro_izq(-15)                                 #gira para acomodarse
